711885f65b150f9db6b923c0d4a13baacb902a86
AnlieJams <98762736+AnlieJams@users.noreply.github.com>
2023-01-31 10:02:02 +0200

    Add files via upload

---
+++ Структура.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+    struct Pizza 
+    {
+        float Cheese;
+        float Sausage;
+        float Tomato;
+        float Ketchup;
+        float Butter;
+    };
+
+    struct Computer 
+    {
+        string CPU;
+        string Motherboard;
+        string VideoCard;
+        int RAM;
+        int StorageDevice;
+    };
+
+
+    struct House 
+    {
+        int Toalets;
+        int Doors;
+        int Rooms;
+        int Stairs;
+        int Windows;
+    };
+
+
+    struct Carpet 
+    {
+        string Figure;
+        string Picture;
+        double Size;
+        double Thickness;
+        string Manufacturer;
+    };
+
+
+    struct ya 
+    {
+        string Name;
+        int Age;
+        int Growth;
+        string Country;
+        int Experience;
+    };
+}
